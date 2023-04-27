73a08fdf5e358cc6145b35922ccc944f0805adf0
Dorian.Bourdon <dorian.bourdontpe@gmail.com>
2023-04-27 21:32:22 +0200

    Add island object

---
+++ OneBlockMC/src/main/java/fr/cercusmc/oneblockmc/islands/Island.java
@@ -0,0 +1,109 @@
+package fr.cercusmc.oneblockmc.islands;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.bukkit.Location;
+import org.bukkit.block.Biome;
+
+public class Island {
+	
+	private UUID owner;
+	private List<String> members;
+	private List<String> bans;
+	private String islandCustomName;
+	private Locations locations;
+	private Biome biome;
+	private IslandStats islandStats;
+	
+	public Island() {}
+	
+	
+	public Island(UUID owner, List<String> members, List<String> bans, String islandCustomName,
+			Locations locations, Biome biome, IslandStats islandStats) {
+		this.owner = owner;
+		this.members = members;
+		this.bans = bans;
+		this.islandCustomName = islandCustomName;
+		this.islandStats = islandStats;
+		this.locations = locations;
+		this.biome = biome;
+	}
+
+
+	public UUID getOwner() {
+		return owner;
+	}
+
+
+	public void setOwner(UUID owner) {
+		this.owner = owner;
+	}
+
+
+	public List<String> getMembers() {
+		return members;
+	}
+
+
+	public void setMembers(List<String> members) {
+		this.members = members;
+	}
+
+
+	public List<String> getBans() {
+		return bans;
+	}
+
+
+	public void setBans(List<String> bans) {
+		this.bans = bans;
+	}
+
+
+	public String getIslandCustomName() {
+		return islandCustomName;
+	}
+
+
+	public void setIslandCustomName(String islandCustomName) {
+		this.islandCustomName = islandCustomName;
+	}
+
+
+	public Locations getLocations() {
+		return locations;
+	}
+
+
+	public void setLocations(Locations locations) {
+		this.locations = locations;
+	}
+
+
+	public Biome getBiome() {
+		return biome;
+	}
+
+
+	public void setBiome(Biome biome) {
+		this.biome = biome;
+	}
+
+
+	public IslandStats getIslandStats() {
+		return islandStats;
+	}
+
+
+	public void setIslandStats(IslandStats islandStats) {
+		this.islandStats = islandStats;
+	}
+	
+	
+	
+	
+	
+	
+
+}
---
+++ OneBlockMC/src/main/java/fr/cercusmc/oneblockmc/islands/Locations.java
@@ -0,0 +1,55 @@
+package fr.cercusmc.oneblockmc.islands;
+
+import org.bukkit.Location;
+
+public class Locations {
+	
+	private Location centerIsland;
+	private Location warpIsland;
+	private Location homeIsland;
+	private Location spawnIsland;
+	
+	public Locations(Location centerIsland, Location warpIsland, Location homeIsland, Location spawnIsland) {
+		this.centerIsland = centerIsland;
+		this.warpIsland = warpIsland;
+		this.homeIsland = homeIsland;
+		this.spawnIsland = spawnIsland;
+	}
+
+	public Location getCenterIsland() {
+		return centerIsland;
+	}
+
+	public void setCenterIsland(Location centerIsland) {
+		this.centerIsland = centerIsland;
+	}
+
+	public Location getWarpIsland() {
+		return warpIsland;
+	}
+
+	public void setWarpIsland(Location warpIsland) {
+		this.warpIsland = warpIsland;
+	}
+
+	public Location getHomeIsland() {
+		return homeIsland;
+	}
+
+	public void setHomeIsland(Location homeIsland) {
+		this.homeIsland = homeIsland;
+	}
+
+	public Location getSpawnIsland() {
+		return spawnIsland;
+	}
+
+	public void setSpawnIsland(Location spawnIsland) {
+		this.spawnIsland = spawnIsland;
+	}
+	
+	
+	
+	
+
+}
